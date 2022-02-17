1d792a29caf87fe105620b6edd987811bb47d84e
RJLimBU <kevinlim@bu.edu>
2022-02-16 23:31:35 -0500

    add testcase for deviceapi

--- deviceAPI.py
+++ deviceAPI.py
@@ -1,25 +1,47 @@
 # skeleton code of device API
 from collections import namedtuple
 from typing import NamedTuple
+import json
 
 class DeviceInfo(NamedTuple):
 	name: str
 	deviceType: str
+	unit: str
 	data: list
 
 class Status(NamedTuple):
 	success: bool
 	error: str
 
-def readData(deviceInfo, key):
-	if deviceInfo is None:
-		return Status(success=False, error="No Device Info")
+apiKey = [
+	"903810847",
+	"890183781",
+	"812321901",
+	"328948027",
+	"438028901",
+	"432901890"
+]
 
-	if type(key) is not str:
+def readData(filename, key):
+
+	try:
+		f = open(filename)
+		data = json.load(f)
+	except:
+		return Status(success=False, error="unable to load file")
+
+	if key not in apiKey:
 		return Status(success=False, error="Incorrect api Key")
 
-	if type(deviceInfo.data) is not list:
-		return Status(success=False, error="No Data Found")
+	for device in data["devices"]:
+
+		if device is None:
+			return Status(success=False, error="No Device Info")
+
+		if device["data"] is not list:
+			return Status(success=False, error="No Data Found")
+
+		if device["unit"] is None:
+			return Status(success=False, error="unit not found")
 
 	return Status(success=True, error="")
-
--- tests/test_DeviceAPI.py
+++ tests/test_DeviceAPI.py
@@ -1,17 +1,28 @@
+#import deviceAPI
 from deviceAPI import *
 
 def test_deviceapi1():
-	bloodDevice = DeviceInfo("bloodPressureMachine","blood_pressure_device", [120])
-	key = "4329018"
+	#bloodDevice = DeviceInfo("bloodPressureMachine","blood_pressure_device", [120])
+	key = "432901890"
+	filename = "deviceinfo.json"
 
-	status = readData(bloodDevice, key)
+	status = readData(filename, key)
 
 	assert status.success == True and status.error == ""
 
 def test_deviceapi2():
-	bloodDevice = DeviceInfo("bloodPressureMachine","blood_pressure_device", [120])
-	key = 4329018
+	#bloodDevice = DeviceInfo("bloodPressureMachine","blood_pressure_device", [120])
+	key = "4329018"
+	filename = "deviceinfo.json"
 
-	status = readData(bloodDevice,key)
+	status = readData(filename,key)
 
 	assert status.success == False and status.error == "Incorrect api Key"
+
+def test_deviceapi3():
+	key = "890183781"
+	filename = "deviceinfo_f"
+
+	status = readData(filename, key)
+
+	assert status.success == False and status.error == "unit not found"
